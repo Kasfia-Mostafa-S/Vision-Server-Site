4e390963f72079f8f96cc1d27cd3e0e066ce3128
Kasfia-Mostafa-S <kasfiamostafa.s@gmail.com>
2024-02-22 15:44:55 +0600

    product added

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ijwgr8d.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -17,58 +16,55 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-
     const userCollection = client.db("visionDB").collection("user");
     const productCollection = client.db("visionDB").collection("products");
 
-
     app.post("/users", async (req, res) => {
-        const user = req.body;
-        const query = { email: user.email };
-        const existingUser = await userCollection.findOne(query);
-        if (existingUser) {
-          return res.send({ message: "user already exist", insertedId: null });
-        }
-        const result = await userCollection.insertOne(user);
-        res.send(result);
-      });
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exist", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
 
-      app.get("/users", async (req, res) => {
-        const result = await userCollection.find().toArray();
-        res.send(result);
-      });
+    app.get("/products", async (req, res) => {
+      const result = await productCollection.find().toArray();
+      res.send(result);
+    });
 
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
 
-      app.get("/products", async (req, res) => {
-        const result = await productCollection.find().toArray();
-        res.send(result);
-      });
-
-      app.get("/products/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
-        const result = await productCollection.findOne(query);
-        res.send(result);
-      });
-  
-
-
-
-
-
-
+    app.post("/products", async (req, res) => {
+      const productItem = req.body;
+      const result = await productCollection.insertOne(productItem);
+      res.send(result);
+    });
 
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -83,6 +79,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Vision is setting on post ${port}`);
 });
-
-
-
