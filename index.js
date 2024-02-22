de6dafb23cbda1177cce8414c17c91f019e22ba9
Kasfia-Mostafa-S <kasfiamostafa.s@gmail.com>
2024-02-22 13:17:25 +0600

    corrected objectId

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
@@ -10,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ijwgr8d.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -36,6 +35,12 @@ async function run() {
           return res.send({ message: "user already exist", insertedId: null });
         }
         const result = await userCollection.insertOne(user);
+        res.send(result);
+      });
+
+
+      app.get("/users", async (req, res) => {
+        const result = await userCollection.find().toArray();
         res.send(result);
       });
 
